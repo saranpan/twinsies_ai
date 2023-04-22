4384a2efaa327b247a8833b4095273383d945399
Wallik2 <runpan4edu@gmail.com>
2023-04-22 11:02:54 +0700

    debug

--- app.py
+++ app.py
@@ -71,6 +71,10 @@ if selected == 'Predict':
             top3_person = list(infer_dct.keys())[:3]
             top3_diff = list(infer_dct.values())[:3]
             st.text(top3_person)
+            st.text(os.listdir("database"))
+            st.text(os.listdir("database/cardi b"))
+            image = Image.open("database/cardi b/2.jpg")    
+            st.image())
         
         st.balloons()
         _, you, _ = st.columns((1,1,1))
